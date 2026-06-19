bc76b017bfc3ab64fe4eeeffbb17ec11909074eb
Hoodie-Spirit <seyajure.24@est.ucab.edu.ve>
2026-06-19 01:02:07 -0400

    terminado SensorHilo

--- FitManage/src/main/java/com/fitmanage/smartgym/model/SensorHilo.java
+++ FitManage/src/main/java/com/fitmanage/smartgym/model/SensorHilo.java
@@ -3,9 +3,7 @@ package com.fitmanage.smartgym.model;
 import com.fitmanage.smartgym.controller.GymController;
 import java.util.Random;
 
-// TODO: Issue #2 - Modificar la firma de la clase para que pueda ejecutarse en un hilo secundario
-
-public class SensorHilo {
+public class SensorHilo implements Runnable {
     private final String tipoSensor; // Recibe "TEMPERATURA" o "HUMEDAD"
     private final RegistroCentral modelo;
     private final GymController controlador;
@@ -17,11 +15,37 @@ public class SensorHilo {
         this.controlador = controlador;
     }
 
+    @Override
+    public void run(){
+        boolean flip = true;
+        while(flip){
+            switch (tipoSensor){
+                case "temperatura":
+                    modelo.registrarTemperatura(Math.round(10 * (15 + (Math.random() * 46))) / 10.0);
+                    controlador.actualizarPantalla();
+                    break;
+                case "humedad":
+                    modelo.registrarHumedad(Math.round(10 * (10 + (Math.random() * 91))) / 10.0);
+                    controlador.actualizarPantalla();
+                    break;
+                default:
+                    System.out.println("Error al registrar un tipo de sensor");
+                    controlador.actualizarPantalla();
+                    break;
+            }
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+    }
 // TODO: Issue #2 - Sobrescribir el método de ciclo de vida del hilo
 // Requisitos:
 // 1. Crear un ciclo infinito 'while' controlado.
 // 2. Si 'tipoSensor' es "TEMPERATURA", generar un aleatorio entre 15 y 45 y llamar a modelo.registrarTemperatura().
 // 3. Si 'tipoSensor' es "HUMEDAD", generar un aleatorio entre 10 y 90 y llamar a modelo.registrarHumedad().
-// 4. Invocar el método controlador.actualizarPantalla() para gatillar el refresco visual.
+// 4. Invocar el metodo controlador.actualizarPantalla() para gatillar el refresco visual.
 // 5. Utilizar Thread.sleep() para pausar la ejecución del hilo de forma asíncrona.
 }
